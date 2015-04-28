613746bd78134ae27e827c647db559684e2fd5df
Kamel Arab <kamel.arab@fia-net.com>
2015-04-28 18:16:47 +0200

    Add test datepickerService$

--- test/spec/service/datePickerSrvSpec.js
+++ test/spec/service/datePickerSrvSpec.js
@@ -6,7 +6,6 @@ describe('Unit : Testing datepickerSrv Service', function(){
 
     var factory;
 
-
     beforeEach(module('md-datepicker.service'));
     beforeEach(inject(function (_datepickerSrv_, $locale, $filter) {
         factory = _datepickerSrv_;
@@ -35,26 +34,94 @@ describe('Unit : Testing datepickerSrv Service', function(){
         expect(angular.isFunction(factory.getMountDto)).toBe(true);
     });
 
+    it('should getMountDto of mount april 2015 get mounth name and year', function(){
 
-  /*  it('should return an answer', function() {
-        var result = factory.initAnswer(questions);
-        var numberAnswer = 0;
-        angular.forEach(result, function(value, key){
-            numberAnswer =numberAnswer + 1;
-        });
+        var dateMine = new Date(2015, 3, 1);
+        var dateMax = new Date(2015, 3, 30);
+        var test = factory.getMountDto(dateMine,dateMax);
 
-        expect(result.currentPage).toBe(1);
-        expect(numberAnswer).toBe(questions.length);
+        expect(test.length).toBe(1);
+        expect(test[0].name).toEqual("avril");
+        expect(test[0].mounth).toEqual(3);
+        expect(test[0].year).toEqual(2015);
     });
 
-    it('Check Double luck', function() {
-        var result = factory.initAnswer(questions);
-        result['5'] = "test 1234";
-        expect(factory.isDoubleLuck(1,result)).toBe(true);
-        result['5'] = "";
-        expect(factory.isDoubleLuck(1,result)).toBe(false);
-        result['19'] = "test 1234";
-        expect(factory.isDoubleLuck(1,result)).toBe(false);
+    it('should getMountDto of mount april 2015 first day week is undenied object with week begin monday', function(){
+
+        var dateMine = new Date(2015, 3, 1);
+        var dateMax = new Date(2015, 3, 30);
+        var test = factory.getMountDto(dateMine,dateMax);
+        expect(test[0].weeks[0][0]).toBe(undefined);
     });
-*/
+
+    it('should getMountDto of mount april 2015 second day week is undenied object with week begin monday', function(){
+
+        var dateMine = new Date(2015, 3, 1);
+        var dateMax = new Date(2015, 3, 30);
+        var test = factory.getMountDto(dateMine,dateMax);
+
+        expect(test[0].weeks[0][1]).toBe(undefined);
+
+    });
+
+    it('should getMountDto  of mount april 2015 third day week is object with week begin monday', function(){
+
+        var premierAvril ={
+            n:1,
+            day: "mercredi",
+            enabled: true
+        };
+
+        var dateMine = new Date(2015, 3, 1);
+        var dateMax = new Date(2015, 3, 30);
+        var test = factory.getMountDto(dateMine,dateMax);
+        expect(test[0].weeks[0][2]).toEqual(premierAvril);
+    });
+
+    it('should have a getIndexOfMounthDto function', function() {
+        expect(angular.isFunction(factory.getIndexOfMounthDto)).toBe(true);
+    });
+
+    it('should getIndexOfMounthDto return index of date', function(){
+
+        var dateMine = new Date(2015, 3, 1);
+        var date = new Date(2015, 3, 20);
+        var dateMax = new Date(2016, 3, 30);
+        var test = factory.getMountDto(dateMine,dateMax);
+
+        expect(factory.getIndexOfMounthDto(test,date)).toBe(0);
+
+    });
+
+    it('should getIndexOfMounthDto return index of date 2', function(){
+
+        var dateMine = new Date(2015, 3, 1);
+        var date2 = new Date(2016, 1, 20);
+        var dateMax = new Date(2016, 3, 30);
+        var test = factory.getMountDto(dateMine,dateMax);
+
+        expect(factory.getIndexOfMounthDto(test,date2)).toBe(10);
+
+    });
+
+    it('should getIndexOfMounthDto return index -1 error', function(){
+
+        var dateMine = new Date(2015, 3, 1);
+        var date3 = new Date(2011, 1, 20);
+        var dateMax = new Date(2016, 3, 30);
+        var test = factory.getMountDto(dateMine,dateMax);
+
+        expect(factory.getIndexOfMounthDto(test,date3)).toBe(-1);
+
+    });
+    it('should getIndexOfMounthDto return index -1 error', function(){
+
+        var dateMine = new Date(2015, 3, 1);
+
+        var dateMax = new Date(2016, 3, 30);
+        var test = factory.getMountDto(dateMine,dateMax);
+
+        expect(factory.getIndexOfMounthDto(test,"25/01/2016")).toBe(-1);
+    });
+
 });
